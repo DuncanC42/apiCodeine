74157d91d58fd65ee52693544dc2360301761341
Huntshi <tribottetimeo@gmail.com>
2025-03-20 17:04:32 +0100

    (test): ajout d'un user en bdd

---
+++ src/Entity/User.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="`user`")
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $apiToken;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $derniere_connexion;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getApiToken(): ?string
+    {
+        return $this->apiToken;
+    }
+
+    public function setApiToken(?string $apiToken): self
+    {
+        $this->apiToken = $apiToken;
+
+        return $this;
+    }
+
+    public function getDerniereConnexion(): ?\DateTimeInterface
+    {
+        return $this->derniere_connexion;
+    }
+
+    public function setDerniereConnexion(?\DateTimeInterface $derniere_connexion): self
+    {
+        $this->derniere_connexion = $derniere_connexion;
+
+        return $this;
+    }
+}
